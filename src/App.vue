84c869e398e24d89a602b22ac6da4eadd363e61f
wanghe <945665444@qq.com>
2019-04-27 16:57:04 +0800

    这是第一次添加的东西

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+        <footer-guide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script>
+import footerGuide from './components/FooterGuide/FootGuide'
+    export default {
+        data() {
+            return {
+            }
+        },
+        components:{
+            footerGuide
+        },
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  #app
+    width:100%
+    height 100%
+    background-color #f5f5f5
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 入口文件
+import Vue from 'vue'
+import App from './App'
+import router from './router/index'
+
+
+
+new Vue({
+    el:'#app',
+    router,
+    render:h=>h(App)
+})
